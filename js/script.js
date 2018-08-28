4a51ca4481ff52fa15fe146a26772377839bf2a0
John Yonard Paully <john.paully@avicallinc.com>
2018-08-28 11:57:52 +0800

    Saturday commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+window.onload = function (event) {
+
+    var jyp_modal = document.getElementsByClassName('jyp-modal')
+    var modal_btn = document.getElementsByClassName('btn-gold')
+    var modal_close = document.getElementsByClassName('close')
+
+    for (let i = 0; i <= jyp_modal.length; i++) {
+        modal_btn[i].onclick = () => {
+            jyp_modal[i].style.display = "block"
+        }
+    
+        modal_close[i].onclick = () => {
+            jyp_modal[i].style.display = "none"
+        }
+    
+        if (event.target == jyp_modal[i]) {
+            jyp_modal[i].style.display = "none"
+        }
+    }
+
+    jyp_modal[i].onclick = () => {
+        jyp_modal[i].style.display = "none"
+    }
+}
